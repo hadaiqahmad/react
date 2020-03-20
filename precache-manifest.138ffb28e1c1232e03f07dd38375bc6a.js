d9ed1317623fb208dd68857d2fd7fe5949576dcf
Hadaiq Ahmad <14bscshahmad@seecs.edu.pk>
2020-03-20 11:21:17 +0500

    Updates

---
+++ precache-manifest.138ffb28e1c1232e03f07dd38375bc6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a83d9a9181d6cafd002e0f4024092b6",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "798ee003acba5741c76d",
+    "url": "/react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b17a4ffc2f09d2f1a139",
+    "url": "/react/static/js/2.f36b9c84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react/static/js/2.f36b9c84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "798ee003acba5741c76d",
+    "url": "/react/static/js/main.a3ab1177.chunk.js"
+  },
+  {
+    "revision": "45853a1903f1d63f1add",
+    "url": "/react/static/js/runtime-main.23941f7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react/static/media/logo.5d5d9eef.svg"
+  }
+]);
