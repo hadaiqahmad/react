6b6baa1aed43a2b1e7a47daee6bd29ecd28eb908
Hadaiq Ahmad <14bscshahmad@seecs.edu.pk>
2020-03-20 11:49:19 +0500

    Updates

---
+++ precache-manifest.3f8ee5327f2c103558c50695835cbe88.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e146f606977e0f69891c995d5a94d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc31a6ba25b9c6705743",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "44cdef26b3bfca0a6657",
+    "url": "/static/css/main.92f3a0a8.chunk.css"
+  },
+  {
+    "revision": "dc31a6ba25b9c6705743",
+    "url": "/static/js/2.36e3d7cc.chunk.js"
+  },
+  {
+    "revision": "a1e12f97867665430e5c8e39fc9f180a",
+    "url": "/static/js/2.36e3d7cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44cdef26b3bfca0a6657",
+    "url": "/static/js/main.dbcc2c61.chunk.js"
+  },
+  {
+    "revision": "de569f22836c5e6cc14f",
+    "url": "/static/js/runtime-main.e111ef77.js"
+  },
+  {
+    "revision": "9cfb95ecd7113ad63a7ef21033a997a7",
+    "url": "/static/media/Hadaiq_Ahmad---2.9cfb95ec.png"
+  },
+  {
+    "revision": "343226c58530beaca9a48b8c0f67e36d",
+    "url": "/static/media/about-me.343226c5.jpg"
+  },
+  {
+    "revision": "f2180a83f853056a77be05080a74fe5a",
+    "url": "/static/media/bg-image-about.f2180a83.jpg"
+  }
+]);
